a7138e0f1a7cac08eb98ccbd11eebf44f20c88b3
YassmineRomdhane <yassmineromdhane1996@gmail.com>
2021-02-02 19:12:37 +0100

    add reset buttom

--- app.js
+++ app.js
@@ -4,6 +4,11 @@
       var block =document.getElementById(id)
       console.log(id);
       if(block.innerHTML!=="O" && block.innerHTML !=="X"){
+          for(var i=0 ; i <playBoard.length ; i++){
+            // console.log(playBoard[0][i]===playBoard[1][i]===playBoard[2][i]==='X', playBoard);
+            if(playBoard[0][i]=== 'X' && playBoard[1][i]==='X' && playBoard[2][i]==='X' ){
+                console.log("win");
+            } }
         counter += 1;
       if (counter % 2 === 0) {
           block.innerHTML = "O";
@@ -16,12 +21,26 @@
    }
 
 
- 
+//    var block0 = document.getElementById('block0').setAttribute('value', 'X');
+
+   var playBoard = []
+for(var i=0; i<3; i++) {
+    playBoard[i] = new Array(3);
+
+}
+console.log(playBoard[0]);
 
 
- 
-  
-//   var player1 = "O";
-//   var player2 = "X";
-//   var playboard=[]
+
+
+
+
+
+
+
+
+
+
+
+
  
