d1ea930c98f6c31be09e0bc3e205bef6153e4c98
YassmineRomdhane <yassmineromdhane1996@gmail.com>
2021-02-02 16:23:25 +0100

    checked if board is empty

--- app.js
+++ app.js
@@ -3,21 +3,17 @@
   function marker(id) {
       var block =document.getElementById(id)
       console.log(id);
-      counter += 1;
       if(block.innerHTML!=="O" && block.innerHTML !=="X"){
+        counter += 1;
       if (counter % 2 === 0) {
           block.innerHTML = "O";
       } else {
           block.innerHTML = "X";
       }
-      console.log(counter); 
+
     }
-
-
-
-
-
-  }
+   
+   }
 
 
  
