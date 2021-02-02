c3239caad616eb3f898170a2ce580e8479a5fa7f
YassmineRomdhane <yassmineromdhane1996@gmail.com>
2021-02-02 15:56:08 +0100

    checked if board is empty

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 
-  var cells = document.querySelectorAll(".block");
-  for (var cell of cells) {
-      cell.addEventListener('click', marker)
-  }
-  
-  var counter = 0;
-  function marker() {
+   var counter = 0;
+  function marker(id) {
+      var block =document.getElementById(id)
+      console.log(id);
       counter += 1;
+      if(block.innerHTML!=="O" && block.innerHTML !=="X"){
       if (counter % 2 === 0) {
-          this.innerHTML = "O";
+          block.innerHTML = "O";
       } else {
-          this.innerHTML = "X";
+          block.innerHTML = "X";
       }
+      console.log(counter); 
+    }
+
+
+
+
+
   }
 
+
+ 
+
+
+ 
   
+//   var player1 = "O";
+//   var player2 = "X";
+//   var playboard=[]
+ 
